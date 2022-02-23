8ce78630eb0351502d3ddb7803ee9df8e5e3476c
Fredrik Eriksson <eriksson.nf@gmail.com>
2022-02-23 14:38:35 +0100

    Add functionality to StudenRest

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,69 @@
 package se.iths.rest;
 
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Optional;
+
+@Path("students")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
 
+    @Inject
+    StudentService studentService;
 
+    @Path("")
+    @POST
+    public Response createStudent(Student student){
+
+        studentService.createStudent(student);
+        return Response.ok(student).build();
+    }
+
+    @Path("")
+    @PUT
+    public Response updateStudent(Student student) {
+        studentService.updateStudent(student);
+        return Response.ok(student).build();
+    }
+
+    @Path("lastname")
+    @GET
+    public Response getStudent(@QueryParam("lastname") String lastName){
+
+        List<Student> foundStudent = studentService.findStudentByLastName(lastName);
+
+        if (foundStudent.isEmpty())
+            throw new NotFoundException("No student with lastname: " + lastName + " was found.");
+
+        return Response.ok(foundStudent).build();
+    }
+    @Path("")
+    @GET
+    public Response getAllStudents(@QueryParam("orderBy") List<String> orderBy) {
+
+        List<Student> foundItems = studentService.getAllStudents();
+        return Response.ok(foundItems).build();
+
+    }
+
+    @Path("{id}")
+    @DELETE
+    public Response deleteStudent(@PathParam("id") Long id){
+
+        Optional<Student> foundStudent = studentService.getStudentById(id);
+
+        if (foundStudent.isEmpty())
+            throw new NotFoundException("No student with id: " + id + " was found.");
+
+        studentService.deleteStudent(id);
+
+        return Response.ok(foundStudent).build();
+    }
 }
